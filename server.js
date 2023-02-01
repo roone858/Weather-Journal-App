ea0a4558711a425b217b65f6075d7ca420aabd4f
rooney <roone858@gmail.com>
2023-02-01 13:53:33 +0200

    add readme.md file

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const port = 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static("website"));
+
+app.use(express.static("website")); //to run file in website folder on server
 
 let projectData = {};
 
 app.post("/add", function (req, res) {
-  projectData = req.body
-
+  projectData = req.body;
 });
 
 app.get("/db", function (req, res) {
   res.send(projectData);
 });
 
-const port = 3000;
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`server running on port: ${port}`);
 });
